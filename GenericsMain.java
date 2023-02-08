cd9ac0bef27b16230673352846ba051d6035c595
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-02-09 00:53:50 +0800

    init lab

--- GenericsMain.java
+++
@@ -1,8 +0,0 @@
-public class GenericsMain {
-  public static void main(String[] args) {
-    // instantiate an instance of A<T>, with type argument `Integer`
-    
-    // instantiate an instance of A, without any type argument
-  
-  }
-}
--- Q8.java
+++ Q8.java
@@ -1,4 +1,4 @@
 /**
- * Write a generic class Q8<T> that contains a field that is an array of 
+ * Write a generic class D<T> that contains a field that is an array of 
  * type T with 10 elements. Instantiate that array in the constructor.
  */
